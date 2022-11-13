eaf2c304ee1efb55db1ca517d573007aa8d8ebd4
martynakulis <kulismartyna@gmail.com>
2022-11-13 20:14:53 +0100

    task 2

--- js/task-01.js
+++ js/task-01.js
@@ -1,25 +1,10 @@
-const categories = document.querySelectorAll('.item');
-const categoriesQuantity = categories.length;
+const categoriesQuantity = document.querySelectorAll('h2').length;
 const message = `Number of categories: ${categoriesQuantity}`;
 console.log(message);
 
-const firstCategory = document.querySelector('.item');
-const firstCategoryTitle = firstCategory.firstElementChild.textContent;
-console.log(`Category: ${firstCategoryTitle}`);
-
-const firstElementQuantity = firstCategory.lastElementChild.children.length;
-console.log(`Elements: ${firstElementQuantity}`);
-
-const secondCategory = firstCategory.nextElementSibling;
-const secondCategoryTitle = secondCategory.firstElementChild.textContent;
-console.log(`Category: ${secondCategoryTitle}`);
-
-const secondElementQuantity = secondCategory.lastElementChild.children.length;
-console.log(`Elements: ${secondElementQuantity}`);
-
-const thirdCategory = secondCategory.nextElementSibling;
-const thirdCategoryTitle = thirdCategory.firstElementChild.textContent;
-console.log(`Category: ${thirdCategoryTitle}`);
-
-const thirdElementQuantity = thirdCategory.lastElementChild.children.length;
-console.log(`Elements: ${thirdElementQuantity}`);
+const listItems = document.querySelectorAll('.item');
+for (const item of listItems) {
+  const categoryTitle = item.firstElementChild.textContent;
+  const elementsQuantity = item.querySelectorAll('li').length;
+  console.log(`Category: ${categoryTitle} \n Elements: ${elementsQuantity}`);
+}
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const list = document.querySelector('ul');
+
+ingredients.forEach(ingredient => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  list.append(listItem);
+  console.log(listItem);
+});
