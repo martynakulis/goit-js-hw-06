f22922e13463f21646946de0f18b4f3cb28f5d20
martynakulis <kulismartyna@gmail.com>
2022-11-20 20:57:04 +0100

    finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const spanText = document.querySelector('.color');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanText.textContent = getRandomHexColor();
+});
